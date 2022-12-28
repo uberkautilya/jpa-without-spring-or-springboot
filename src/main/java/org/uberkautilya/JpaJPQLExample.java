60b3b26af44d586f5c4a2be8bff350d71bca3a61
Mithun <mithuncm@gmail.com>
2022-12-28 19:04:33 +0530

    Freeform and Custom Queries with TypedQuery<Object[]> when selecting distinct column values from multiple entities

--- src/main/java/org/uberkautilya/JpaJPQLExample.java
+++ src/main/java/org/uberkautilya/JpaJPQLExample.java
@@ -5,7 +5,6 @@ import org.uberkautilya.entity.Employee;
 import javax.persistence.*;
 import java.util.List;
 
-import static java.lang.Boolean.TRUE;
 
 public class JpaJPQLExample {
     public static void main(String[] args) {
@@ -31,13 +30,32 @@ public class JpaJPQLExample {
         System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
 
         /**
-         * The below caused an error as true was getting converted into 1 for some reason - Todo
+         * The below caused an error as true was getting converted into 1 for some reason - Todo not yet figured out
          * select e from Employee e join AccessCard a on e.id = a.id where a.isActive= true - also does the similar function
          */
         TypedQuery<Employee> entityManagerJoinQuery = entityManager.createQuery(
                 "select e from Employee e where e.accessCard.firmwareVersion = 32", Employee.class);
         List<Employee> joinQueryResultList = entityManagerJoinQuery.getResultList();
         joinQueryResultList.forEach(System.out::println);
+        System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+
+        /**
+         * Custom Query Results: Single return value type
+         */
+        TypedQuery<String> entityManagerCustomQuery = entityManager.createQuery(
+                "select e.name from Employee e where e.accessCard.firmwareVersion = 32", String.class);
+        List<String> customQueryResultList = entityManagerCustomQuery.getResultList();
+        customQueryResultList.forEach(System.out::println);
+        System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
+
+        /**
+         * Custom Query Results: Array of types return object - can query from multiple entities at the same time
+         */
+        TypedQuery<Object[]> query = entityManager.createQuery(
+                "select e.name, e.age, e.dob, a.issueDate from Employee e, AccessCard a where e.accessCard.id = a.id and a.firmwareVersion = 32", Object[].class);
+        // Will contain a list of object arrays
+        List<Object[]> customTypeQueryResultList = query.getResultList();
+        customTypeQueryResultList.forEach(x -> System.out.println("Name: " + x[0] + "| Age: " + x[1] + "| dob: " + x[2] + "| issueDate: " + x[3]));
         System.out.println("°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°");
 
         entityManager.close();
